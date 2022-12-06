b6532c2bf1ec303d0214c8e83b61097355efdf55
Almatov-Nurs <bbekmamatov1991@gmail.com>
2022-12-06 18:35:46 +0600

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import usersReducer from "../redux/slice/registerSlice";
 import code from "../redux/slice/codeSlice";
 import citySlice from "./slice/citySlice";
+import ProductsSlice from "./slice/productsSlice";
 
 export const store = configureStore({
     reducer: {
         usersProfile: usersReducer,
         codeMail: code,
         city: citySlice,
+        products: ProductsSlice,
     }
 });
