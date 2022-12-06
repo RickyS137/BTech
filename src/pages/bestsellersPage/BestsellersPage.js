a4bf9d3e28c34773f69cd3fb6bba7dc0f66fccd2
RickyS137 <em.bazarkylov@gmail.com>
2022-12-06 23:54:01 +0600

    emir5

--- src/pages/bestsellersPage/BestsellersPage.js
+++ src/pages/bestsellersPage/BestsellersPage.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BestsellersCard from '../../components/bestsellers/bestsellersCard/BestsellersCard';
-import { getProducts, ProductsSelect } from '../../redux/slice/productsSlice';
+import {
+  getProducts,
+  LoadSelect,
+  ProductsSelect,
+} from '../../redux/slice/productsSlice';
 import '../../App.css';
 import { Pagination } from '@mui/material';
-import style from './css/bestsellerspage.module.css'
+import style from './css/bestsellerspage.module.css';
 
 const BestsellersPage = () => {
   const products = useSelector(ProductsSelect);
+  const load = useSelector(LoadSelect);
   const dispatch = useDispatch();
 
   const [page, setPage] = useState(1);
@@ -19,15 +24,26 @@ const BestsellersPage = () => {
   return (
     <div className={style.box}>
       <div className="container">
-      <h2>Хиты продаж</h2>
+        <h2>Хиты продаж</h2>
         <ul className={style.products}>
-          {products.slice((page * 20) - 20, (page * 20)).map((item, i) => (
-            <li><BestsellersCard item={item}/></li>
-            ))}
+          {load
+            ? 'loading...'
+            : products.slice(page * 20 - 20, page * 20).map((item, i) => (
+                <li key={i}>
+                  <BestsellersCard item={item} />
+                </li>
+              ))}
         </ul>
-            <Pagination count={Math.ceil(products.length / 20)} onChange={(_,e) => {
-              setPage(e)
-            }}/>
+        {!load && (
+          <div className={style.pagination}>
+            <Pagination
+              size='large'
+              color="primary"
+              count={Math.ceil(products.length / 20)}
+              onChange={(_, e) => setPage(e)}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
