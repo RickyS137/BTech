6f5571ab5471041ecead51af5235d4f486d94e7d
Almatov-Nurs <bbekmamatov1991@gmail.com>
2022-12-22 19:39:27 +0600

    btech

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import classes from "./css/mainPage.module.css"
+import Slider from "../../components/mainPage/slider/Slider";
 
 const MainPage = () => {
     return (
-        <div className={classes.mainPage}></div>
+        <div className={classes.mainPage}>
+            <Slider/>
+        </div>
     );
 };
 
