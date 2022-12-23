ddebbc144fa5a31e5c0619e254906021cc8a4d60
Dilkhan04 <dilhan.04.e@gmail.com>
2022-12-23 17:54:12 +0600

    DilkhanImgMainPage1

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -2,9 +2,7 @@ import React from "react";
 import classes from "./css/mainPage.module.css";
 
 const MainPage = () => {
-    return <div className={classes.mainPage}>
-        <div></div>
-    </div>;
+    return <div className={classes.mainPage}></div>;
 };
 
 export default MainPage;
